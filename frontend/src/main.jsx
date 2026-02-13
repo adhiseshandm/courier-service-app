0b71bef18e813bd951ae27526b484ef6128b9be4
unknown <adhiseshhh@gmail.com>
2026-02-13 15:27:44 +0530

    fix: restore missing imports in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,7 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 import GlobalErrorBoundary from './GlobalErrorBoundary';
 
 createRoot(document.getElementById('root')).render(
